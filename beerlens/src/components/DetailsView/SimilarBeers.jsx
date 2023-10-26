53ff52a3641f29242b6e945612cb4481bf38b8a9
Hanna Snarberg <hannasnaris@gmail.com>
2023-10-26 14:54:46 +0200

    Add horizontal scroller similar beers

--- beerlens/src/components/DetailsView/SimilarBeers.jsx
+++ beerlens/src/components/DetailsView/SimilarBeers.jsx
@@ -5,6 +5,15 @@ const SimilarBeers = () => {
   return (
     <div id='similarBeers' className='similarBeersContainer'>
       <h1 className='similarBeersHeader'>You may also like</h1>
+      <div class='scrolling-wrapper'>
+        <tbody>
+          {[...Array(6)].map((x, i) => (
+            <div class='card' key={i}>
+              <h2>Card</h2>
+            </div>
+          ))}
+        </tbody>
+      </div>
     </div>
   );
 };
