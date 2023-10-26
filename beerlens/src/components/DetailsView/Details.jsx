49f337ad8d37a489fdadf8629c8a3cbfc20e9447
Frida J <97102212+frjda@users.noreply.github.com>
2023-10-26 10:41:42 +0200

    Update Details.jsx

--- beerlens/src/components/DetailsView/Details.jsx
+++ beerlens/src/components/DetailsView/Details.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import DetailsOverview from "./DetailsOverview";
 import TasteDescription from "./TasteDescription";
 import Pairings from "./Pairings";
 import About from "./About";
+import NavBar from "./NavBar";
 
 const Details = () => {
   return (
     <div>
       <DetailsOverview />
+      <NavBar />
       <TasteDescription />
       <hr />
       <Pairings />
