fa8a9f132be7129ab722ab18ee96eb1f9cdc095e
Hanna Snarberg <hannasnaris@gmail.com>
2023-10-26 13:34:58 +0200

    Add reviews

--- beerlens/src/components/DetailsView/components/SmallReviewCard.jsx
+++ beerlens/src/components/DetailsView/components/SmallReviewCard.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React from 'react';
+import '../styles/SmallReviewCard.css';
+import { FaUserCircle } from 'react-icons/fa';
+import { Rating } from '@mui/material';
+import StarIcon from '@mui/icons-material/Star';
 
-const SmallReviewCard = () => {
-  return <div>small</div>;
+const SmallReviewCard = ({ name, rating, text }) => {
+  return (
+    <div className='reviewCardContainer'>
+      <div className='reviewCardRaterInfo'>
+        <FaUserCircle size={39} />
+        <div className='reviewCardNameAndRating'>
+          <div className='reviewCardName'>{name}</div>
+          <Rating
+            name='read-only'
+            value={rating}
+            precision={0.1}
+            size='small'
+            readOnly
+            emptyIcon={
+              <StarIcon style={{ opacity: 0.55 }} fontSize='inherit' />
+            }
+          />
+        </div>
+      </div>
+      <div className='reviewCardText'>{text}</div>
+    </div>
+  );
 };
 
 export default SmallReviewCard;
