10243fffc069f86a8205e518b0d339b7c53f5e0e
Mervan Kaya <mckaya@kth.se>
2023-10-26 16:30:03 +0200

    Alot of progress

---
+++ beerlens/src/components/ExploreView/BeerItem.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import './styles/BeerItem.css';
+import { FlagIcon } from 'react-flag-kit';
+import "@fontsource/inter";
+
+
+export class BeerInfo {
+  constructor(id, BeerIcon, name, brewery, country, countryCode, volume, abv, price, type) {
+    this.id = id;
+    this.BeerIcon = BeerIcon;
+    this.name = name;
+    this.brewery = brewery;
+    this.country = country;
+    this.countryCode = countryCode;
+    this.volume = volume;
+    this.abv = abv;
+    this.price = price;
+    this.type = type;
+  }
+}
+
+
+export function BeerGrid({ rows, cols, onButtonClick, beers }) {
+  const handleClick = (beer) => {
+    if (onButtonClick) {
+      onButtonClick(beer);
+    }
+  };
+
+  const renderItem = (beer, row, col) => (
+    <div
+      key={`${row}-${col}`}
+      className="BeerItem"
+      onClick={() => handleClick(beer)}
+      style={{ cursor: 'pointer' }}  // Added cursor style to indicate it's clickable
+    >
+      {beer ? (
+      <>
+        <div className="Info-Image">
+          <div className='Info'>
+            <div>{beer.type}</div>
+            <div>{beer.name}</div>
+            <div>{beer.brewery}</div>
+            <div className='country-with-flag'>
+              <FlagIcon code={beer.countryCode} style={{ width: '15px', height: '10 px' }} />
+              <span>{beer.country}</span>
+            </div>
+
+            <div className="BeerItem-info">
+              <div>{beer.volume}</div>
+              <div>{beer.abv}</div>
+              <div>{beer.price}</div>
+            </div>
+
+          </div>
+          
+          <img src={beer.BeerIcon} alt=''/>
+        </div>
+
+        <button className="AddToCart-btn">Add to Cart</button>
+      </>
+    ) : (
+      `${row},${col}`
+    )}
+  </div>
+);
+
+  const renderRow = (row) => {
+    const beerRow = beers ? beers[row] : null;
+    return (
+      <div key={row} className="BeerGrid-row">
+        {Array.from({ length: cols }, (_, col) => {
+          const beer = beerRow ? beerRow[col] : null;
+          return renderItem(beer, row, col);
+        })}
+      </div>
+    );
+  };
+
+  return <div className="BeerGrid">{Array.from({ length: rows }, (_, row) => renderRow(row))}</div>;
+}
--- beerlens/src/components/ExploreView/utils.js
+++ beerlens/src/components/ExploreView/utils.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/BeerType.css';
-import './styles/Explore.css';
+import './styles/Dropdown.css';
 import { useState } from 'react';
 
 export function ImageButton(onclick_path, image_path) {
@@ -20,37 +20,6 @@ export function ImageButton(onclick_path, image_path) {
         </button>
     );
 }
-
-export function ButtonMatrix({ rows, cols, onButtonClick, labels }) {
-  const handleClick = (row, col) => {
-    if (onButtonClick) {
-      onButtonClick(row, col);
-    }
-  };
-
-  const renderButton = (row, col) => (
-    <button
-      key={`${row}-${col}`}
-      className="filter-button"
-      onClick={() => handleClick(row, col)}
-    >
-      {labels && labels[row] && labels[row][col] ? labels[row][col] : `${row},${col}`}
-    </button>
-  );
-
-  const renderRow = (row) => (
-    <div key={row} className="matrix-row">
-      {Array.from({ length: cols }, (_, col) => renderButton(row, col))}
-    </div>
-  );
-
-  return (
-    <div className="matrix">
-      {Array.from({ length: rows }, (_, row) => renderRow(row))}
-    </div>
-  );
-}
-
 
 export function Dropdown() {
 
