332083b879b62a8ed71857daeb7946bc3e26468d
Hanna Snarberg <hannasnaris@gmail.com>
2023-10-31 16:19:24 +0100

    Change grid system explore page

--- beerlens/src/components/ExploreView/BeerData.js
+++ beerlens/src/components/ExploreView/BeerData.js
@@ -1,26 +1,105 @@
 import { BeerInfo } from './BeerItem';
-import Image from '../../assets/images/brewdog_punk_ipa.png'
-
+import Image from '../../assets/images/brewdog_punk_ipa.png';
 
 const BeerData = [
-  [
-    new BeerInfo(Image, 'Beer Name 1', 'Brewery A', 'USA', 'US', '500 ml', 5, 4.99, 'Ale', 'Regular', 'Fruity', 'Bottle'),
-    new BeerInfo(Image, 'Beer Name 2', 'Brewery B', 'Germany', 'DE', '330 ml', 4.5, 3.99, 'Lager', 'Upcoming', 'Crisp', 'Can'),
-  ],
-  [
-    new BeerInfo(Image, 'Beer Name 3', 'Brewery D', 'USA', 'US', '500 ml', 6, 5.99, 'IPA', 'Seasonal', 'Hoppy', 'Bottle'),
-    new BeerInfo(Image, 'Beer Name 4', 'Brewery E', 'United Kingdom', 'GB', '330 ml', 4, 4.49, 'Pilsner', 'Regular', 'Malty', 'Can'),
-  ],
-  [
-    new BeerInfo(Image, 'Beer Name 5', 'Brewery G', 'France', 'FR', '750 ml', 7.5, 8.99, 'Saison', 'Seasonal', 'Spicy', 'Bottle'),
-    new BeerInfo(Image, 'Beer Name 6', 'Brewery H', 'Canada', 'CA', '330 ml', 5, 4.59, 'Bock', 'Upcoming', 'Rich', 'Can'),
-  ],
-  [
-    new BeerInfo(Image, 'Beer Name 7', 'Brewery J', 'USA', 'US', '500 ml', 5.5, 5.99, 'Ale', 'Regular', 'Sweet', 'Bottle'),
-  ]
+  new BeerInfo(
+    Image,
+    'Beer Name 1',
+    'Brewery A',
+    'USA',
+    'US',
+    '500 ml',
+    5,
+    4.99,
+    'Ale',
+    'Regular',
+    'Fruity',
+    'Bottle'
+  ),
+  new BeerInfo(
+    Image,
+    'Beer Name 2',
+    'Brewery B',
+    'Germany',
+    'DE',
+    '330 ml',
+    4.5,
+    3.99,
+    'Lager',
+    'Upcoming',
+    'Crisp',
+    'Can'
+  ),
+  new BeerInfo(
+    Image,
+    'Beer Name 3',
+    'Brewery D',
+    'USA',
+    'US',
+    '500 ml',
+    6,
+    5.99,
+    'IPA',
+    'Seasonal',
+    'Hoppy',
+    'Bottle'
+  ),
+  new BeerInfo(
+    Image,
+    'Beer Name 4',
+    'Brewery E',
+    'United Kingdom',
+    'GB',
+    '330 ml',
+    4,
+    4.49,
+    'Pilsner',
+    'Regular',
+    'Malty',
+    'Can'
+  ),
+  new BeerInfo(
+    Image,
+    'Beer Name 5',
+    'Brewery G',
+    'France',
+    'FR',
+    '750 ml',
+    7.5,
+    8.99,
+    'Saison',
+    'Seasonal',
+    'Spicy',
+    'Bottle'
+  ),
+  new BeerInfo(
+    Image,
+    'Beer Name 6',
+    'Brewery H',
+    'Canada',
+    'CA',
+    '330 ml',
+    5,
+    4.59,
+    'Bock',
+    'Upcoming',
+    'Rich',
+    'Can'
+  ),
+  new BeerInfo(
+    Image,
+    'Beer Name 7',
+    'Brewery J',
+    'USA',
+    'US',
+    '500 ml',
+    5.5,
+    5.99,
+    'Ale',
+    'Regular',
+    'Sweet',
+    'Bottle'
+  ),
 ];
 
-
-  
-  export default BeerData;
-  
+export default BeerData;
--- beerlens/src/components/ExploreView/BeerItem.js
+++ beerlens/src/components/ExploreView/BeerItem.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './styles/BeerItem.css';
 import { FlagIcon } from 'react-flag-kit';
-import "@fontsource/inter";
+import '@fontsource/inter';
 import { useState } from 'react';
 
-
 export class BeerInfo {
-  constructor(BeerIcon, name, brewery, country, countryCode, volume, abv, price, type, assortment_type, flavor_profile, package_type) {
+  constructor(
+    BeerIcon,
+    name,
+    brewery,
+    country,
+    countryCode,
+    volume,
+    abv,
+    price,
+    type,
+    assortment_type,
+    flavor_profile,
+    package_type
+  ) {
     this.id = (name + '-' + brewery).replace(/\s+/g, '-');
     this.BeerIcon = BeerIcon;
     this.name = name;
@@ -23,11 +35,9 @@ export class BeerInfo {
   }
 }
 
-export function BeerGrid({ rows, cols, onButtonClick, beers }) {
-
+export function BeerGrid({ onButtonClick, beers }) {
   const [selectedBeerId, setSelectedBeerId] = useState(null);
 
-  
   const handleClick = (beer) => {
     setSelectedBeerId(beer.id);
     if (onButtonClick) {
@@ -37,7 +47,7 @@ export function BeerGrid({ rows, cols, onButtonClick, beers }) {
   };
 
   const addItemToCart = (beer, event) => {
-    event.stopPropagation();  // Prevent click event from bubbling up to the BeerItem
+    event.stopPropagation(); // Prevent click event from bubbling up to the BeerItem
     console.log('Added to cart:', beer);
   };
 
@@ -46,58 +56,44 @@ export function BeerGrid({ rows, cols, onButtonClick, beers }) {
     return (
       <div
         key={`${row}-${col}`}
-        className="BeerItem"
+        className='BeerItem'
         onClick={() => handleClick(beer)}
         style={{ cursor: 'pointer' }}
       >
         <>
-          <div className="Info-Image">
+          <div className='Info-Image'>
             <div className='Info'>
               <div>{beer.type}</div>
               <div>{beer.name}</div>
               <div>{beer.brewery}</div>
               <div className='country-with-flag'>
-                <FlagIcon code={beer.countryCode} style={{ width: '15px', height: '10 px' }} />
+                <FlagIcon
+                  code={beer.countryCode}
+                  style={{ width: '15px', height: '10 px' }}
+                />
                 <span>{beer.country}</span>
               </div>
-              <div className="BeerItem-info">
+              <div className='BeerItem-info'>
                 <div>{beer.volume}</div>
                 <div>{beer.abv}%</div>
                 <div>${beer.price}</div>
               </div>
             </div>
-            <img src={beer.BeerIcon} alt=''/>
+            <img src={beer.BeerIcon} alt='' />
           </div>
-          <button className="AddToCart-btn" onClick={(e) => addItemToCart(beer, e)}>
-          Add to Cart
+          <button
+            className='AddToCart-btn'
+            onClick={(e) => addItemToCart(beer, e)}
+          >
+            Add to Cart
           </button>
           {selectedBeerId === beer.id}
-      </>
-    </div>
-  );
-};
-  
-  
-  const renderPlaceholder = (key) => {
-    return <div key={key} className="BeerItem" style={{ visibility: 'hidden' }} />;
-  };
-
-  const renderRow = (row) => {
-    const beerRow = beers ? beers[row] : null;
-    if (!beerRow) return null;
-
-    // Ensure there are always 3 elements in a row, adding placeholders if necessary
-    const items = [...beerRow];
-    while (items.length < 3) {
-      items.push(null);
-    }
-
-    return (
-      <div key={row} className="BeerGrid-row">
-        {items.map((beer, col) => beer ? renderItem(beer, row, col) : renderPlaceholder(`${row}-${col}`))}
+        </>
       </div>
     );
   };
 
-  return <div className="BeerGrid">{Array.from({ length: rows }, (_, row) => renderRow(row))}</div>;
+  return (
+    <div className='BeerGrid'>{beers.map((beer) => renderItem(beer))}</div>
+  );
 }
