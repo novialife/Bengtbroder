8e4fd07103acef0f84e7fbf97642b1648e68e515
Filip Isak Mattsson <85882037+fi-m@users.noreply.github.com>
2023-10-31 17:30:14 +0100

    Update About.jsx
    Skrev lite skit 👯

--- beerlens/src/components/AboutView/About.jsx
+++ beerlens/src/components/AboutView/About.jsx
@@ -55,15 +55,17 @@ const About = () => {
     },
     {
       name: 'Filip Mattsson',
-      mail: 'mailto:hannasn@kth.se',
-      linkedin: null,
+      mail: 'fim@kth.se',
+      linkedin: 'https://www.linkedin.com/in/fim/',
       role: (
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate.
+          My primary contribution was in the design and implementation of the recommendation engine, 
+          ensuring that users receive tailored beer suggestions based on their preferences and past interactions. 
+          This involved intricate data analysis, algorithm development, and continuous optimization 
+          to enhance user experience and satisfaction. Additionally, I helped shape the business model, 
+          strategizing on how to monetize the platform while ensuring user retention.
+          My efforts was concentrated around bridging the gap between technology and business to
+          create a holistic and user-centric platform.
         </p>
       ),
       image: profileImage,
